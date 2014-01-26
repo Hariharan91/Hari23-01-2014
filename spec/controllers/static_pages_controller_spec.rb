24a81dc5834a954eb76ba43fdcfbecbeef3c5b3a
Hariharan <nhary91@gmail.com>
2014-01-26 18:08:52 +0530

    Trial turortials

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,11 +1,20 @@
 require 'spec_helper'
 
 describe StaticPagesController do
+	describe "home page" do
+		it "should have the content 'home'" do
+			visit '/statoc_pages/home'
+			expect(page).to have_content('home')
+		end
 
+		it "should have the title 'home'" do
+			visit '/static_pages/home'
+			expect(page).to have_title ("Hari app")
+end
 	describe "help page" do
 		it "should have the content 'help'" do
       visit '/static_pages/help'
-      expect(page).to have_content('help)
+      expect(page).to have_content('help')
     end
 
     it "should have the title 'help'" do
